041107e09ad021d59660f0b65cdc3b944ced1350
the-praxs <ps4534@nyu.edu>
2022-02-19 22:41:18 -0500

    Solutions uploaded for exercises 1.2 and 1.3

---
+++ Chapter 1/1.2.cpp
@@ -0,0 +1,11 @@
+/*
+    Change the program to return -1. A return value of -1 is often treated 
+    as an indicator that the program failed. Recompile and rerun your 
+    program to see how your system treats a failure indicator from main.
+*/
+
+#include <iostream>
+
+int main() {
+    return -1;
+}
